ec0f7ea46caa164527d3672d2624e4a484676a66
John Smith <slimcoin@anche.no>
2015-02-21 11:25:44 -0500

    Changed functions to comply with emacs' namespace coding convention

--- autoinsertion_guru.el
+++ autoinsertion_guru.el
@@ -10,7 +10,7 @@
 
 (defcustom aig-template-dirs (list "~/.emacs.d/aig-templates")
   "The directories where aig templates are search for in."
-  :type (repeat :args (directory) :tag "List of directories")
+  :type '(repeat :args (directory) :tag "List of directories")
   :group 'autoinsertion-guru)
 ;;
 ;; User customizable variables
@@ -20,9 +20,9 @@
 ;; Some initial setup
 ;;
 
-;;make a hash table test named string= using string=
+;;make a hash table test named aig--string= using string=
 ;; it is a bit more efficient than using equal
-(define-hash-table-test 'string= 'string= 'sxhash)
+(define-hash-table-test 'aig--string= 'string= 'sxhash)
 
 ;;
 ;; Some initial setup
@@ -31,7 +31,7 @@
 ;;
 ;; Global variables
 ;;
-(defvar hash-templates-by-mode (make-hash-table :test 'string=)
+(defvar aig--hash-templates-by-mode (make-hash-table :test 'aig--string=)
   "A hash table that holds a list of hash tables of loaded 
 templates for each respective mode identified by the key
 
@@ -39,7 +39,7 @@ Each hash table element in each of the lists contained in this hash table is
 guaranteed to have the following fields: name, hook, delim, expr
 If on loading, those fields are missing, an error will be signaled")
 
-(defvar list-context-hash-templates '() 
+(defvar aig--list-context-hash-templates '() 
   "A list of hashes of templates that are enabled in the current context
 This variable is updated every time the context is updated")
 
@@ -48,13 +48,13 @@ This variable is updated every time the context is updated")
 ;;
 
 ;;Read the contents of a file as a list of string where each string is a line of the file
-(defun read-lines (file-path)
+(defun aig--read-lines (file-path)
   "Return a list of lines of a file at file-path."
   (with-temp-buffer
     (insert-file-contents file-path)
     (split-string (buffer-string) "\n" t)))
 
-(defun unescape-string (str)
+(defun aig--unescape-string (str)
   "Takes a string with escape chars and makes them into their equivalents processed
 ie: \n (2 chars) becomes newline (1 char)"
   ;;add quotes around the string so that read interprets the entire string as one sexp
@@ -62,7 +62,7 @@ ie: \n (2 chars) becomes newline (1 char)"
 
 ;;Parses the header of a template file and saves the parsed values in the hash-table
 ;; Handles errors as needed
-(defun parse-template-file-header (contents hash-table)
+(defun aig--parse-template-file-header (contents hash-table)
   (if (null contents) ;;if contents is null, that means there was no #-- end identifier
       (error "Missing end identifier to template file header")
     (let ((first (car contents))
@@ -85,21 +85,21 @@ ie: \n (2 chars) becomes newline (1 char)"
                  ;;Be sure to unescape the string so that chars like \n actually become newlines
                  ;; this makes it as though whatever is between the [] is interpreted as a normal string
                  ;; although being read character by character from the file input
-                 (puthash key (unescape-string val) hash-table))
+                 (puthash key (aig--unescape-string val) hash-table))
 
-               (parse-template-file-header rest hash-table))))
+               (aig--parse-template-file-header rest hash-table))))
           ((string-match "^#\\s-*--\\s-*$" first) ;;end identifier matching
            (cons hash-table rest)) ;;Return the hash table and the rest of the file's contents when done
           (t ;;Else, there must be an error
            (error "Invalid syntax on line: \"%s\"" first))))))
 
-(defun parse-template-file (template-file-path)
+(defun aig--parse-template-file (template-file-path)
   "Given a template file path, this function parses it"
-  (let ((contents (read-lines template-file-path))
-        (hash-table (make-hash-table :test 'string=)))
+  (let ((contents (aig--read-lines template-file-path))
+        (hash-table (make-hash-table :test 'aig--string=)))
     ;;Each hash should guarantee to have the following fields: name, hook, delim, expr, exec
     ;; in the rest of the program, so do the error checking here
-    (let* ((pair (parse-template-file-header contents hash-table))
+    (let* ((pair (aig--parse-template-file-header contents hash-table))
            (hash-template (car pair))
            (rest-contents (cdr pair)))
       ;;concatenate rest-contents with newlines after each
@@ -117,15 +117,15 @@ ie: \n (2 chars) becomes newline (1 char)"
       ;;If mapc did not encounter an error, hash-template is valid and can be returned
       hash-template)))
 
-(defun load-template-file (mode template-file-path)
+(defun aig--load-template-file (mode template-file-path)
   "Loads a template file into a given mode"
   ;;Get what is in mode, defaulting to '() if there is nothing
   ;; and append the parsed data to that list and place it back into the hash table
-  (let ((mode-contents (gethash mode hash-templates-by-mode '())))
-    (puthash mode (cons (parse-template-file template-file-path) mode-contents)
-             hash-templates-by-mode)))
+  (let ((mode-contents (gethash mode aig--hash-templates-by-mode '())))
+    (puthash mode (cons (aig--parse-template-file template-file-path) mode-contents)
+             aig--hash-templates-by-mode)))
 
-(defun get-mode-dirs (root-dir)
+(defun aig--get-mode-dirs (root-dir)
   "Gets all of the directories in root-dir that end in -mode"
   ;;root-contents is a list of files and directories ending in -mode with its full path
   ;; they are then later filtered to only directories by the attributes
@@ -138,7 +138,7 @@ ie: \n (2 chars) becomes newline (1 char)"
                root-contents
                :initial-value '())))
 
-(defun get-template-files (mode-dir)
+(defun aig--get-template-files (mode-dir)
   "Given a directory mode-dir, returns all of the files in it
 It is assumed every file in mode-dir is a template file"
   (let ((templates (directory-files-and-attributes mode-dir t nil t)))
@@ -150,15 +150,12 @@ It is assumed every file in mode-dir is a template file"
                templates
                :initial-value '())))
 
-(defun load-templates-from-root-dir (root-dir)
+(defun aig--load-templates-from-root-dir (root-dir)
   "Loads all of the template files located in the sub-mode dirs of root-dir and returns nothing
 root-dir is expected to be an existing directory of all of the sub-mode dirs"
 
-  ;;Clear the hash table every time the load function is called
-  (clrhash hash-templates-by-mode)
-
   ;;mode-dirs is a list of the full paths of directories containing templates
-  (let ((mode-dirs (get-mode-dirs root-dir)))
+  (let ((mode-dirs (aig--get-mode-dirs root-dir)))
     (mapc #'(lambda (mode-dir) 
               "mode-dir is the full directory path of each sub-directory in 
 root-dir that will contain template files"
@@ -166,28 +163,34 @@ root-dir that will contain template files"
               ;;mode is the mode each template file is to be activated in which is
               ;; the directory name of mode-dir without the full path
               ;; it is extracted by spliting the mode-dir about /, taking the last one via (car (last ...))
-              (let ((template-files (get-template-files mode-dir))
+              (let ((template-files (aig--get-template-files mode-dir))
                     (mode (car (last (split-string mode-dir "/")))))
                 (mapc #'(lambda (template-file-path)
-                          (load-template-file mode template-file-path))
+                          (aig--load-template-file mode template-file-path))
                       template-files)))
           mode-dirs)))
 
-(defun load-templates-from-dirs (dirs-ls)
+(defun aig--load-templates-from-dirs* (dirs-ls)
   "Loads all of the templates found in a list of directories"
   (if (null dirs-ls)
       nil ;;base case satisfied, return
     (let ((first (car dirs-ls))
           (rest (cdr dirs-ls)))
-      (load-templates-from-root-dir first) ;;load each individual directory
-      (load-templates-from-dirs rest)))) ;;continue the recursion
+      (aig--load-templates-from-root-dir first) ;;load each individual directory
+      (aig--load-templates-from-dirs* rest)))) ;;continue the recursion
 
-(defun get-templates-for-major-mode ()
+(defun aig-load-templates-from-dirs (dirs-ls)
+  "Clears the hash templates and loads all the templates found in a list of directories"
+  ;;Clear the hash table every time the load function is called
+  (clrhash aig--hash-templates-by-mode)
+  (aig--load-templates-from-dirs* dirs-ls))
+
+(defun aig--get-templates-for-major-mode ()
   "Returns the list of template hash tables corresponding to the respective
 value in major-mode. Returns '() if there are no templates"
-  (gethash (symbol-name major-mode) hash-templates-by-mode '()))
+  (gethash (symbol-name major-mode) aig--hash-templates-by-mode '()))
 
-(defun aig-get-search-region (start-delim)
+(defun aig--get-search-region (start-delim)
   "Gets the region of the buffer that is between the regex start-delim and point.
 If start-delim is not satisfied, the beginning of the buffer is used"
   ;;make it a special let because point moves around when calling re-search-backward
@@ -206,21 +209,21 @@ If start-delim is not satisfied, the beginning of the buffer is used"
 (defun aig-update-context ()
   "Updates the context list, hooked on post-command-hook"
   
-  ;;clear the current context by setting list-context-hash-templates to '()
-  (setq list-context-hash-templates '())
+  ;;clear the current context by setting aig--list-context-hash-templates to '()
+  (setq aig--list-context-hash-templates '())
 
   ;;templates-ls is the list of templates for the current major-mode, '() if there are none
-  (let ((templates-ls (get-templates-for-major-mode)))
+  (let ((templates-ls (aig--get-templates-for-major-mode)))
     (mapc #'(lambda (hash-template)
-             (let ((search-area (aig-get-search-region (gethash "delim" hash-template))))
+             (let ((search-area (aig--get-search-region (gethash "delim" hash-template))))
                ;;Destructive cons the hash-template if expr matches the context search-area
                ;; else do nothing
                (if (string-match (gethash "expr" hash-template) search-area)
-                   (push hash-template list-context-hash-templates)
+                   (push hash-template aig--list-context-hash-templates)
                  nil)))
           templates-ls)))
 
-(defun hook-matches-last-input-event (hook)
+(defun aig--hook-matches-last-input-event (hook)
   "Returns t if the last-input-event matches the hook, else it returns nil"
   (cond
    ((symbolp last-input-event)
@@ -237,26 +240,23 @@ If start-delim is not satisfied, the beginning of the buffer is used"
           (rest (cdr context-hashes)))
 
       ;;check if the hook matched
-      (if (hook-matches-last-input-event (gethash "hook" first))
+      (if (aig--hook-matches-last-input-event (gethash "hook" first))
           (cons first (aig-scan-context rest)) ;;cons the matched context template and continue the recursion
-          (aig-scan-context rest)) ;;continue the recursion 
-      )))
+          (aig-scan-context rest))))) ;;continue the recursion 
 
-(defun eval-hash-template (hash-template)
+(defun aig--eval-hash-template (hash-template)
   (let ((exec-str (gethash "exec" hash-template)))
     (eval (read exec-str))))
 
-(defun aig-post-command-hook-handle ()
-  (let* ((scanned-contexts (aig-scan-context list-context-hash-templates))
+(defun aig--post-command-hook-handle ()
+  (let* ((scanned-contexts (aig-scan-context aig--list-context-hash-templates))
          (contexts-len (length scanned-contexts)))
-
     ;;handles if there was a match
     (cond
      ((zerop contexts-len) nil) ;;no matches
-     ((= 1 contexts-len) (eval-hash-template (car scanned-contexts))) ;;single match, execute it
+     ((= 1 contexts-len) (aig--eval-hash-template (car scanned-contexts))) ;;single match, execute it
      ;TODO: make the prompting
-     (t (message "Multiple matches found:")) ;;more than one match, prompt for which one to use
-     ))
+     (t (message "Multiple matches found:")))) ;;more than one match, prompt for which one to use
 
   ;;update the context after the scanning of satisfied hooks is complete
   (aig-update-context))
@@ -264,9 +264,10 @@ If start-delim is not satisfied, the beginning of the buffer is used"
 ;(char-to-string 10)
 ;(read-event)
 
-(add-hook 'post-command-hook #'aig-post-command-hook-handle)
-(remove-hook 'post-command-hook #'aig-post-command-hook-handle)
+(add-hook 'post-command-hook #'aig--post-command-hook-handle)
+(remove-hook 'post-command-hook #'aig--post-command-hook-handle)
 
-(load-templates-from-root-dir "/home/damian/bin/ELisp_files/autoinsertion_guru/")
+(aig-load-templates-from-dirs '("/home/damian/bin/ELisp_files/autoinsertion_guru/"))
 
+;;Provide that this extension was loaded
 (provide 'autoinsertion-guru)
